b9d212fb0525ef8fe7954b410cd34e51676e5315
Mauro Berlanda <m.berlanda@fit2you.it>
2015-12-09 10:30:58 +0100

    Create contact page (ex. 3.2)

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -24,4 +24,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About | #{@base_title}"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+
 end
